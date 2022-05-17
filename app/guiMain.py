98389d2cec363c73b4e56c5ccd2bbd0f88db0994
hafid4827 <luceres12joshua@gmail.com>
2022-05-16 22:17:21 -0500

    hola mundo

---
+++ app/guiMain.py
@@ -0,0 +1,8 @@
+import eel
+eel.init('web')
+""" iria la logica """
+
+
+
+
+eel.start('main.html', size=(400, 800))
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./app/web/css/*.{html,js}", "./app/web/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
